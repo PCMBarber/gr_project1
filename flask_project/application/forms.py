8ad3047cfdede3e9ccc3cadeb11517406641129e
Sean <s.j.oconnor@hotmail.co.uk>
2021-09-07 11:48:25 +0100

    updates to flask

--- flask_project/application/forms.py
+++ flask_project/application/forms.py
@@ -1,22 +1,19 @@
-from gr_project1.flask_project.application.models import Games
-from gr_project1.flask_project.application.routes import games
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length
+from application.models import Games, Mission_List
 
-from application.models import Todos
-# add all the classes in here
 
-class GamesForm(FlaskForm):
-    game = StringField('Add a game here',
-        validators = [DataRequired()])
-    submit = SubmitField('Submit')
+# class Add_GamesForm(FlaskForm):
+#     game = StringField('Add a game here',
+#         validators = [DataRequired(), Length(min=1, max=30)])
+#     submit = SubmitField('Games List')
 
-    def validate_task(self, game):
-        games = Games.query.all()
-        for game in games:
-            if game.task == game.data:
-                raise ValidationError('This game exists already!')
+#     def validate_title(self, game):
+#         games = Games.query.all()
+#         for game in games:
+#             if game == game.data:
+#                 raise ValidationError('This game exists already!')
 
 # class OrderTodo(FlaskForm):
 #     order_with = SelectField('Order With',
