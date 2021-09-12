41b2185b70103c25ebb08b2126fce03fe24f8a9b
Ubuntu <ubuntu@ip-172-31-15-217.eu-west-1.compute.internal>
2021-09-12 12:45:20 +0000

    RDS and Jenkins with DBURI and SCTKEY

--- flask_project/application/__init__.py
+++ flask_project/application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 #('SECRET_KEY')
