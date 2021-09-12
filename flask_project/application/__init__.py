614d4b2a3f12f8f4cbb838e1902fd47c367c0d7e
Ubuntu <ubuntu@ip-172-31-15-217.eu-west-1.compute.internal>
2021-09-12 11:50:40 +0000

    deployment to Jenkins databaseURI setup

--- flask_project/application/__init__.py
+++ flask_project/application/__init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-
-
+#('SECRET_KEY')
+#'sqlite:///app.db'
 db = SQLAlchemy(app)
 
 from application import routes
