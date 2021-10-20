3149ef579a837aab5ba9405659c32456c2532391
Piers Barber <Kochab.96@gmail.com>
2021-10-20 15:18:30 +0100

    -y in wrong place

--- Infrastructure/install.sh
+++ Infrastructure/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install python3 python3-pip gunicorn -y
+sudo apt install -y python3 python3-pip gunicorn
 git clone https://github.com/PCMBarber/gr_project1.git
 cd ./gr_project1/flask_project
 SECRET_KEY=gdsfejhkbljkhgsdrelhjkbdgsrew
